0cdf12286ce351e08a9ecd47c018fe5a7dd21b38
daniel <danielpaltano@gmail.com>
2025-05-24 11:38:09 -0300

    guardar antes de debug

--- src/main/java/ar/com/codoacodo/Auto.java
+++ src/main/java/ar/com/codoacodo/Auto.java
@@ -1,5 +1,7 @@
 package ar.com.codoacodo;
 
+
+
 public class Auto {
 	
 	/*
@@ -7,31 +9,62 @@ public class Auto {
 	 */
 	
 	boolean encendido;
-	float velocidad;
+	float velocidadActual;
 	String marca;
 	String modelo;
 	int anio;
+	float velocidadMaxima;
 	
 	//constructor por defecto explicito
 	Auto(){
+		this.encendido = false;		
+		this.velocidadMaxima = 200;
+		this.velocidadActual = 0;
+	}
+	
+	Auto(final float velocidadMaxima){
+		this.velocidadActual = 0;
+		this.velocidadMaxima = velocidadMaxima;
 		
-		encendido = false;
 	}
 	
 	//metodos
 	
 	void encender() {
-		
+		if(!encendido) {
+			
+			System.out.println("se ha encendido");
 		encendido = true;
+		}else {
+			System.out.println("ya esta encendido");
+		}
 		
 	}
 	void apagar() {
 		
 	}
 	void acelerar() {
+		if(encendido) {
+			
+			if (this.velocidadActual < velocidadMaxima) {
+				System.out.println("acelerando");
+				this.velocidadActual++;
+			}else {
+			System.out.println("se quema el auto");
+		}
+		}else {
+			System.out.println("debe encender el auto");
+		}
 		
-	}
+}
 	void frenar() {
+		if (this.encendido) {
+			if(this.velocidadActual > 0) {
+				System.out.println("frenando");
+				this.velocidadActual--;
+			}
+			
+		}
 		
 	}
 	
---
+++ src/main/java/ar/com/codoacodo/Main2.java
@@ -0,0 +1,40 @@
+package ar.com.codoacodo;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		Auto miPrimerAuto = new Auto(5);
+	
+		
+		//miPrimerAuto.encender();
+		//miPrimerAuto.encender();
+		
+		//miSegundoAuto.encender();
+			
+		miPrimerAuto.acelerar();
+		miPrimerAuto.encender();
+		
+		miPrimerAuto.acelerar();
+		miPrimerAuto.acelerar();
+		miPrimerAuto.acelerar();
+		miPrimerAuto.acelerar();
+		miPrimerAuto.acelerar();
+		
+		System.out.println("fin");
+		System.out.println("ahora frenando");
+		
+		miPrimerAuto.frenar();
+		miPrimerAuto.frenar();
+		miPrimerAuto.frenar();
+		miPrimerAuto.frenar();
+		miPrimerAuto.frenar();
+		
+		miPrimerAuto.frenar();
+		
+		
+
+	}
+
+}
